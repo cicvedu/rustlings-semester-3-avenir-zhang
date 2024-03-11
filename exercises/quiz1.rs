5e010aeb5708e14aa6e53aef4f9a3f592878f882
Avenir Zhang (ext.) <Avenir.Zhang@partner.bmwgroup.com>
2024-03-11 17:52:25 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,10 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nums: usize) -> usize {
+    if nums <= 40 { nums * 2 } else { nums }
+}
 
 // Don't modify this function!
 #[test]
